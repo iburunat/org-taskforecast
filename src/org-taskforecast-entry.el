9a33d550b584c6ed0b8276c915da03a5cc45c4a6
HKey <s06139@gmail.com>
2019-10-22 21:31:02 +0900

    Add source files

---
+++ src/org-taskforecast-entry.el
@@ -0,0 +1,32 @@
+;;; org-taskforecast-entry.el --- Task entry API for org-taskforecast.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Hiroki YAMAKAWA
+
+;; Author:  Hiroki YAMAKAWA <s06139@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;;;; Variables
+
+(defconst org-taskforecast-entry-ref-prop-name "ORG_TASKFORECAST_REFERENCE")
+
+(provide 'org-taskforecast-entry)
+;;; org-taskforecast-entry.el ends here
---
+++ src/org-taskforecast-pkg.el
@@ -0,0 +1,9 @@
+(define-package
+  "org-taskforecast"
+  "0.1.0"
+  "Manage closed task list and forecast time flow with org-mode."
+  '((emacs "24")) )
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
