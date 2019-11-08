e01e4891b51a5adb04ec94bbde5c3bff999eac7a
HKey <s06139@gmail.com>
2019-11-08 14:29:16 +0900

    Add assertion macro

---
+++ src/org-taskforecast-util.el
@@ -0,0 +1,43 @@
+;;; org-taskforecast-util.el --- Utilities for org-taskforecast  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Hiroki YAMAKAWA
+
+;; Author:  Hiroki YAMAKAWA <s06139@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;;;; Debug
+
+(defvar org-taskforecast-enable-assert nil
+  "When non-nil, enable `org-taskforecast-assert'.")
+
+(defmacro org-taskforecast-assert (expr &optional message)
+  "Assert EXPR.
+When EXPR returns nil call error with MESSAGE.
+If `org-taskforecast-enable-assert' is nil, this assertion is disabled."
+  `(when org-taskforecast-enable-assert
+     (unless ,expr
+       (error ,(or message
+                   (format "Assertion failed: %s" expr))))))
+
+
+(provide 'org-taskforecast-util)
+;;; org-taskforecast-util.el ends here
