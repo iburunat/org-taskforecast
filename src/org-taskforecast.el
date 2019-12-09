650e0d8caa065c3251ae668825df5116ba42ae39
HKey <s06139@gmail.com>
2019-12-09 17:11:05 +0900

    Move org-taskforecast-util.el into org-taskforecast.el

--- src/org-taskforecast.el
+++ src/org-taskforecast.el
@@ -24,7 +24,77 @@
 
 ;;; Code:
 
-;; TODO
+(require 'dash)
+
+;;;; Utility
+
+;;; Debug
+
+(defvar org-taskforecast-enable-assert nil
+  "When non-nil, enable `org-taskforecast-assert'.")
+
+(defmacro org-taskforecast-assert (expr &optional message)
+  "Assert EXPR.
+When EXPR returns nil call error with MESSAGE.
+If `org-taskforecast-enable-assert' is nil, this assertion is disabled."
+  `(when org-taskforecast-enable-assert
+     (unless ,expr
+       (error ,(or message
+                   (format "Assertion failed: %s" expr))))))
+
+;;; Alist Utility
+
+(defmacro org-taskforecast-defalist (name fields &optional docstring)
+  "Define an alist type.
+NAME is a name of the alist.
+FIELDS is a list of symbols.
+DOCSTRING is a documentation of defining alist.
+
+This macro defines following functions:
+- Constructor named as NAME
+- Type checker named as NAME-type-p
+
+The constructor is defined like below:
+
+    (org-taskforecast-defalist my-abc (a b c))
+
+    Expands to
+
+    (cl-defun my-abc (&key a b c)
+      (list (cons 'a a)
+            (cons 'b b)
+            (cons 'c c)))
+
+To use the constructor like below:
+
+    (my-abc :a 1 :b 2 :c 3)
+    => ((a . 1)
+        (b . 2)
+        (c . 3))
+
+The type checker returns non-nil when the argument is a valid as
+the defined alist.
+It checks below:
+- the argument is an alist
+- all of keys of the alist are in FIELDS
+- the alist contains all of keys in FIELDS"
+  (declare (indent 2) (doc-string 3))
+  `(progn
+     (defun ,(intern (format "%s-type-p" name)) (x)
+       ,(format "Check an alist is valid as `%s'.
+Non-nil means valid." name)
+       (and (listp x)
+            ,@(--map `(assoc ',it x) fields)
+            t))
+
+     (cl-defun ,name (&key ,@fields)
+       ,(apply #'concat
+               `("Make an alist that contains following fields:"
+                 ,@(--map (format "\n- %s" it) fields)
+                 "\n\n----\n\n"
+                 ,docstring))
+       (list ,@(--map `(cons ',it ,it) fields)))))
+
 
 (provide 'org-taskforecast)
 ;;; org-taskforecast.el ends here
