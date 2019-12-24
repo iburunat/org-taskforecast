6b646475d8d053569d080d7330d8afa65c84f658
HKey <s06139@gmail.com>
2019-12-24 11:19:00 +0900

    Update emacs version of dependency

--- org-taskforecast.el
+++ org-taskforecast.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/HKey/org-taskforecast
 ;; Keywords: convenience
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3") (dash "2.16.0") (s "1.12.0"))
+;; Package-Requires: ((emacs "25") (dash "2.16.0") (s "1.12.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
