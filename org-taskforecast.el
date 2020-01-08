d67a2b2e2976d1c94663b3b49067cd1e246c01a1
HKey <s06139@gmail.com>
2020-01-08 16:58:46 +0900

    Remove alist utilities

--- org-taskforecast.el
+++ org-taskforecast.el
@@ -123,59 +123,6 @@ If `org-taskforecast-enable-assert' is nil, this assertion is disabled."
        (error ,(or message
                    (format "Assertion failed: %s" expr))))))
 
-;;; Alist
-
-(defmacro org-taskforecast-defalist (name fields &optional docstring)
-  "Define an alist type.
-NAME is a name of the alist.
-FIELDS is a list of symbols.
-DOCSTRING is a documentation of defining alist.
-
-This macro defines following functions:
-- Constructor named as NAME
-- Type checker named as NAME-type-p
-
-The constructor is defined like below:
-
-    (org-taskforecast-defalist my-abc (a b c))
-
-    Expands to
-
-    (cl-defun my-abc (&key a b c)
-      (list (cons 'a a)
-            (cons 'b b)
-            (cons 'c c)))
-
-To use the constructor like below:
-
-    (my-abc :a 1 :b 2 :c 3)
-    => ((a . 1)
-        (b . 2)
-        (c . 3))
-
-The type checker returns non-nil when the argument is a valid as
-the defined alist.
-It checks below:
-- the argument is an alist
-- all of keys of the alist are in FIELDS
-- the alist contains all of keys in FIELDS"
-  (declare (indent 2) (doc-string 3))
-  `(progn
-     (defun ,(intern (format "%s-type-p" name)) (x)
-       ,(format "Check an alist is valid as `%s'.
-Non-nil means valid." name)
-       (and (listp x)
-            ,@(--map `(assoc ',it x) fields)
-            t))
-
-     (cl-defun ,name (&key ,@fields)
-       ,(apply #'concat
-               `("Make an alist that contains following fields:"
-                 ,@(--map (format "\n- %s" it) fields)
-                 "\n\n----\n\n"
-                 ,docstring))
-       (list ,@(--map `(cons ',it ,it) fields)))))
-
 ;;; Type
 
 (defun org-taskforecast--encoded-time-p (x)
